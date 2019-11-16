52552bc3c984b00d753ad3543607082bbb74ce49
Lucio Franco <luciofranco14@gmail.com>
2019-11-16 14:36:26 +0100

    wip server

--- examples/redis/server.rs
+++ examples/redis/server.rs
@@ -1,15 +1,18 @@
-use futures::TryStreamExt;
-use std::{collections::HashMap, sync::Arc};
+use futures::{Stream, TryStreamExt};
+use redis_protocol::types::Frame;
+use std::{collections::HashMap, io, sync::Arc};
 use tokio::{
-    io::{AsyncBufReadExt, BufReader},
+    io::{AsyncBufRead, AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::{TcpListener, TcpStream},
     sync::Mutex,
 };
 
-type Error = Box<dyn std::error::Error>;
+type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 #[derive(Clone, Debug, Default)]
-struct Database {}
+struct Database {
+    db: Arc<Mutex<HashMap<String, Frame>>>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -38,18 +41,118 @@ async fn main() -> Result<(), Error> {
 }
 
 impl Database {
-    async fn handle_connection(
-        &mut self,
-        mut conn: TcpStream,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        let (r, w) = conn.split();
+    async fn handle_connection(&mut self, mut conn: TcpStream) -> Result<(), Error> {
+        let (r, mut w) = conn.split();
 
-        let mut lines = BufReader::new(r).lines();
+        let buf = BufReader::new(r);
+        let mut lines = buf.lines();
 
-        let arg = lines.try_next().await?;
+        if let Some(line) = lines.next_line().await? {
+            match &line[0..1] {
+                "*" => {
+                    let mut full_message = line.clone();
+
+                    full_message += "\r\n";
+
+                    let array_len = line[1..].parse::<usize>()?;
+
+                    for _ in 0..array_len {
+                        let type_line = lines.next_line().await?.unwrap();
+                        let body_line = lines.next_line().await?.unwrap();
+
+                        full_message += &type_line;
+                        full_message += "\r\n";
+
+                        full_message += &body_line;
+                        full_message += "\r\n";
+                    }
+
+                    let frame = redis_protocol::decode::decode(full_message.as_bytes()).unwrap();
+
+                    println!("decoded frame {:?}", frame);
+
+                    if let (Some(frame), _) = frame {
+                        match self.handle_request(frame).await {
+                            Ok(Response::Ok) => w.write_all("+Ok\r\n".as_bytes()).await?,
+                            Ok(Response::Frame(f)) => {
+                                let mut buf = vec![0u8; 1024];
+
+                                redis_protocol::encode::encode(&mut buf, &f).unwrap();
+
+                                w.write_all(&buf[..]).await?;
+                            }
+                            Ok(Response::Error(e)) => unimplemented!(),
+                            Err(_e) => {
+                                unimplemented!("implement error message");
+                            }
+                        }
+                    }
+                }
+                _ => unimplemented!("Only array's are supported"),
+            }
+        } else {
+            eprintln!("Connection closed early");
+        }
 
         Ok(())
     }
+
+    async fn handle_request(&mut self, frame: Frame) -> Result<Response, Error> {
+        if let Frame::Array(frames) = frame {
+            let mut frames = frames.into_iter();
+
+            if let Some(Frame::BulkString(s)) = frames.next() {
+                let cmd = String::from_utf8_lossy(&s[..]).into_owned();
+
+                match cmd.to_uppercase().as_str() {
+                    "GET" => {
+                        let key = if let Some(Frame::BulkString(s)) = frames.next() {
+                            String::from_utf8_lossy(&s[..]).into_owned()
+                        } else {
+                            unimplemented!("expected bulkstring for key");
+                        };
+
+                        let db = self.db.lock().await;
+
+                        if let Some(frame) = db.get(&key) {
+                            return Ok(Response::Frame(frame.clone()));
+                        } else {
+                            unimplemented!("key does not exist.")
+                        }
+                    }
+
+                    "SET" => {
+                        let key = if let Some(Frame::BulkString(s)) = frames.next() {
+                            String::from_utf8_lossy(&s[..]).into_owned()
+                        } else {
+                            unimplemented!("expected bulkstring for key");
+                        };
+
+                        let value = if let Some(frame) = frames.next() {
+                            frame
+                        } else {
+                            unimplemented!("PUT expects a value");
+                        };
+
+                        let mut db = self.db.lock().await;
+
+                        db.insert(key, value);
+
+                        return Ok(Response::Ok);
+                    }
+                    _ => unimplemented!("command not supported"),
+                }
+            } else {
+                unimplemented!("Expected array with atleast one item and of type BulkString");
+            }
+        } else {
+            unimplemented!("only outter array frame types are accepted.")
+        }
+    }
 }
 
-// async fn parse_request(conn: &mut TcpStream) -> Result<String, Error> {}
+enum Response {
+    Ok,
+    Frame(Frame),
+    Error(String),
+}
